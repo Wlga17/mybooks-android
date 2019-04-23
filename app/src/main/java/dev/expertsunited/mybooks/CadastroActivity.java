983f68a0a617929f9818264eef10def88a905625
Gabriel Carvalho <gabrielksi@outlook.com>
2019-04-22 22:59:51 -0300

    Botao cadastrar funcionando

---
+++ app/src/main/java/dev/expertsunited/mybooks/CadastroActivity.java
@@ -0,0 +1,14 @@
+package dev.expertsunited.mybooks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/dev/expertsunited/mybooks/MainActivity.java
+++ app/src/main/java/dev/expertsunited/mybooks/MainActivity.java
@@ -1,14 +1,39 @@
 package dev.expertsunited.mybooks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        this.mViewHolder.btnCadastrar = findViewById(R.id.btn_novaConta);
+        this.mViewHolder.btnCadastrar.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.btn_novaConta){
+            Intent intent = new Intent(this, CadastroActivity.class);
+            startActivity(intent);
+
+        }
+    }
+
+    private static class ViewHolder{
+        Button btnCadastrar;
+    }
+
+
+
 }
