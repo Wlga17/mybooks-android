bb6d06771685cf1f9008e351a9df17e067fc2fb5
Gabriel Carvalho <gabrielksi@outlook.com>
2019-04-23 01:44:42 -0300

    Adicionado menu a lista de desejos, e algumas outras alterações

--- app/src/main/java/dev/expertsunited/mybooks/ListaDesejosActivity.java
+++ app/src/main/java/dev/expertsunited/mybooks/ListaDesejosActivity.java
@@ -9,5 +9,6 @@ public class ListaDesejosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_desejos);
+        getSupportActionBar().hide();
     }
 }
